032803e029eda09b40f6c1b307351bd4ec25d1a6
andy.lv <andy.lv.vip@hotmail.com>
2018-02-04 15:27:29 +0800

    create project...

--- src/main/java/aspire/user/App.java
+++ src/main/java/aspire/user/App.java
@@ -1,11 +1,13 @@
 package aspire.user;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("aspire")
-@EnableAutoConfiguration
+@SpringBootApplication
+@EnableEurekaClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
--- src/main/java/aspire/user/controller/IndexController.java
+++ src/main/java/aspire/user/controller/IndexController.java
@@ -1,14 +1,12 @@
 package aspire.user.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String index() {
         return "Welcome to Aspire User Service";
     }
